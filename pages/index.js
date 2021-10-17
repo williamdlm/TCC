3ed9b718bfe869d603204068c22114e44c40074a
William Mota <williammota.david@gmail.com>
2021-10-17 17:11:24 -0300

    Implementando login por google acc

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,39 @@ import Background from "../src/components/Background";
 import Head from "next/head";
 import { MainComplete } from "../src/components/Main";
 import { ExperienceProvider } from "../src/contexts/ExperienceContext";
+import React from "react";
+import styled from "styled-components";
+
+function GoogleLogIn() {
+  const GoogleIn = styled.img`
+    margin: 50vh auto;
+    display: block;
+  `;
+  return (
+    <GoogleIn src="https://res.cloudinary.com/dhmkfekt2/image/upload/v1634500984/btn_google_signin_light_normal_web_gfq5t1.png" />
+  );
+}
+
+function LogOnScreen() {
+  return (
+    <>
+      <Header />
+      <MainComplete />
+    </>
+  );
+}
+
+function LogInScreen() {}
+
+const screenStates = {
+  //Conecte-se
+  LOGIN: "LOGIN",
+  //Entrar
+  LOGON: "LOGON",
+};
 
 export default function Home(props) {
+  const [screenState, setScreenState] = React.useState(screenStates.LOGIN);
   // console.log(props);
   return (
     <ExperienceProvider
@@ -20,8 +51,8 @@ export default function Home(props) {
         <Head>
           <title>TCC</title>
         </Head>
-        <Header />
-        <MainComplete />
+        {screenState === screenStates.LOGIN && <GoogleLogIn />}
+        {screenState === screenStates.LOGON && <LogOnScreen />}
       </Background>
     </ExperienceProvider>
   );
